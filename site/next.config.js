74d9b9093429f687a4648207eb123d350c12c018
yijiangchengming <yijiangchengming@qq.com>
2024-05-14 13:19:15 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/',
   trailingSlash: true,
   images: {
     unoptimized: true,
